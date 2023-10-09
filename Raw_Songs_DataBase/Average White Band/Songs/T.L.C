9704c85c3d8bd63b1dad9ed7597044096c0c6220
Antonia Baies <baies.antonia@gmail.com>
2023-10-09 13:51:55 +0200

    Improved_comments 1

---
+++ Raw_Songs_DataBase/Average White Band/Songs/T.L.C
@@ -0,0 +1,53 @@
+
+When we make music, we gotta make that with a swing
+If we all keep it up, we can all get it out somewhere
+When we make love it ain't no different, no
+All a woman needs is a tender love and care
+
+T L C it's short for tender love and care
+It's all you gotta know (tender love and care)
+But if you don't believe, it's not gonna get you anywhere
+Keep pushing, still got a long way to go
+
+T L C
+And when you're working hard and there ain't much left to give
+You know your love ain't going anywhere
+That woman won't care about how much it takes to live
+If you just get down and give her some of that thing that you got there
+
+T L C
+Tender love and care
+That's all it needs
+T L C
+Tender love and care
+Certain affection guaranteed
+
+And when you feel that groove you better not miss what's going on
+She's giving you the chance you gotta keep giving it all you can
+No stop to think about it 'cause if you leave that girl without it
+She's gonna find her TLC from some other man so keep on giving her
+
+T L C
+Tender love and care
+It's all you need to make it
+And you know she can thank it
+T L C
+Tender love and care
+Whatever you do, you gotta do it right
+
+'Cause you don't leave a woman lonely at night
+
+Here it comes now
+Whatever you do, you gotta do it right, yeah
+T L C
+As if only the woman lonely night
+Gotta give her the action
+T L C
+You wanna get some savage back
+Be the love you got
+T L C
+Do what you can, know where the flame is a heart
+T L C
+Whatever it is much to give
+But whatever it is it's the way to live
+
